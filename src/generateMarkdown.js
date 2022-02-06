89100eff74303c288d206a5cb0d51b89c1bd4e00
jackiem2 <jacqueline.merlan@gmail.com>
2022-02-06 15:03:39 -0600

    added readme template

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,19 +1,36 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test](#test)
+  - [License](#license)
+  - [Contact Info](#contact-info)
+
+  ## Description:
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ## License
+  [![License:](https://img.shields.io/badge/License-${data.license}-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Contact Info
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email} 
 `;
 }
 
