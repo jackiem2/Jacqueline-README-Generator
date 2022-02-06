ef21909c9d8270c7cf1f6c2d96af5d72f0df06d7
jackiem2 <jacqueline.merlan@gmail.com>
2022-02-06 13:53:58 -0600

    added all the acceptance criteria questions

--- index.js
+++ index.js
@@ -18,7 +18,120 @@ const questions = [
                 return false;
             }
         }
-    }
+    },
+    // Description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project. (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+            return true;
+            }else {
+                console.log('Please provide a description for your project!');
+                return false;
+            }
+        }
+    },
+    // Installation
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide instructions to install your project. (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+            return true;
+            }else{
+                console.log('Please provide instructions for your project installation');
+                return false;
+            }
+        }
+    },
+    // Usage
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+            return true;
+            }else{
+                console.log('Please explain how to use your project.')
+                return false;
+            }
+        }
+    },
+    // Contribution
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Are other developers allowed to contribute to your project? (Required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+            return true;
+            }else{
+                console.log('Please explain how other developers may contribute to your project.')
+                return false;
+            }
+        }
+    },
+    // Test
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test your project? (Required)',
+        validate: testInput => {
+            if (testInput) {
+            return true;
+            }else{
+                console.log('Please explain how do you test your project')
+                return false;
+            }
+        }
+    },
+    // License
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Please choose a license for your project. (Required)',
+        choices: ['MIT', 'Mozilla', 'Apache'],
+        validate: licenseInput => {
+            if(licenseInput) {
+            return true;
+            }else{
+                console.log('Please choose a license for your project')
+                return false;
+            }
+        }
+    },
+    // Github username
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username? (Required)',
+        validate: githubInput => {
+            if(githubInput) {
+            return true;
+            }else{
+                console.log('Please enter your github username')
+                return false;
+            }
+        }
+    },
+    // Email Address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+        validate: emailInput => {
+            if(emailInput) {
+            return true;
+            }else{
+                console.log('Please enter your email address.')
+                return false;
+            }
+        }
+    },
 ];
 
 // TODO: Create a function to write README file
@@ -26,8 +139,8 @@ function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
         if (err) throw err;
         console.log('README File created!')
-    })
-}
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {
@@ -35,8 +148,8 @@ function init() {
     .then(function (userInput){
         console.log(userInput)
         writeToFile("README.md", generateMarkdown(userInput));
-    })
-}
+    });
+};
 
 // Function call to initialize app
 init();
